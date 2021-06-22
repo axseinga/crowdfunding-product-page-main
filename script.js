4e5cdd12772e83822deb1ee083e252c47280d065
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-06-22 10:27:05 +0100

    add modal in progress

--- script.js
+++ script.js
@@ -31,4 +31,61 @@ overlay.addEventListener("click", function () {
     "./images/icon-hamburger.svg",
     "./images/icon-close-menu.svg"
   );
+  //modalBackProject.classList.add("hide-modal");
+});
+
+// Open modal - when clicked on 'Back this project'
+
+const backProjectButton = document.querySelector(".intro__cta");
+const modalBackProject = document.querySelector(".modal");
+const overlayModal = document.querySelector(".overlay-modal");
+const modalCloseButton = document.querySelector(".modal__close");
+
+backProjectButton.addEventListener("click", function () {
+  console.log("Clicked");
+  modalBackProject.classList.remove("hide-modal");
+  overlayModal.classList.remove("hidden");
+});
+
+// Hide modal when click on x or on overlay
+
+overlayModal.addEventListener("click", function () {
+  overlayModal.classList.add("hidden");
+  modalBackProject.classList.add("hide-modal");
+});
+
+modalCloseButton.addEventListener("click", function () {
+  overlayModal.classList.add("hidden");
+  modalBackProject.classList.add("hide-modal");
+});
+
+// when card is hover change border color
+
+const cardList = document.querySelectorAll(".card--modal");
+cardList.forEach((card) => {
+  card.addEventListener("mouseover", function () {
+    console.log("Mouse over!!!");
+    card.classList.add("card--hover");
+  });
+});
+
+cardList.forEach((card) => {
+  card.addEventListener("mouseout", function () {
+    console.log("Mouse over!!!");
+    card.classList.remove("card--hover");
+  });
+});
+
+// when card is clicked show checkmark, change border, show card__additional
+
+const modalCheckmark = document.querySelector(".modal__checkmark");
+const cardAddition = document.querySelector(".card__addition");
+
+cardList.forEach((card) => {
+  card.addEventListener("click", function () {
+    console.log("clicked!!!");
+    card.classList.add("card--hover");
+    modalCheckmark.style.display = "block";
+    cardAddition.classList.remove("hide-modal");
+  });
 });
